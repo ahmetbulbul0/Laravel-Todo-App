06ae2fbc0b20266444c23709964e9fa4fcc319cc
Ahmet Bülbül <ahmetbulbul@protonmail.com>
2022-11-03 21:51:11 +0300

    app.css moved, layout edited

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Todo App - {{ $title }}</title>
-    @vite('resources/css/app.css')
+    <link rel="stylesheet" type="text/css" href="{{ URL::asset('/src/css/app.css') }}">
     <script src="https://kit.fontawesome.com/066700f129.js" crossorigin="anonymous"></script>
 </head>
 
